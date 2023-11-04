75bff8c6237b7ee2e02dc884ca898913ed563d15
kanta1207 <kan0712livepool@gmail.com>
2023-11-03 18:10:07 -0700

    Fix : routing

--- routing/src/App.tsx
+++ routing/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './routes/Home';
 import { About } from './routes/About';
 import { Menu } from './routes/Menu';
@@ -8,14 +8,16 @@ import { Order } from './routes/Order';
 import { NotFound } from './routes/NotFound';
 function App() {
   return (
-    <Router>
-      <Route index path="/" Component={Home} />
-      <Route path="/about" Component={About} />
-      <Route path="/menu" Component={Menu} />
-      <Route path="/contact" Component={Contact} />
-      <Route path="/order" Component={Order} />
-      <Route Component={NotFound} />
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" Component={Home} />
+        <Route path="/about" Component={About} />
+        <Route path="/menu" Component={Menu} />
+        <Route path="/contact" Component={Contact} />
+        <Route path="/order" Component={Order} />
+        <Route Component={NotFound} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
